7a6101cd42941dbb1c837d75c5cb2c599012742d
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-06-08 01:32:52 +0000

    Added hyprpaper and linux-wpe compat fixç

--- linux-wpe/toggle-wpe.sh
+++ linux-wpe/toggle-wpe.sh
@@ -4,6 +4,7 @@ pids=($(pgrep -fa "linux-wallpaperengine"))
 
 if [[ ${#pids[@]} -eq 0 ]]; then
   echo "Did not find WPE process to kill."
+  sleep 0.5
   $HOME/.dotfiles/linux-wpe/random-wp.sh
 else
   $HOME/.dotfiles/linux-wpe/kill-wp.sh
